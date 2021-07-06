7abfc1da57dc14fb590f810e4608f5e0b097f128
Kadar Basha Azad <86721497+Zero-7860@users.noreply.github.com>
2021-07-07 00:27:43 +0530

    Add files via upload
    uploading quotes database

---
+++ Lyricist/quotes.py
@@ -0,0 +1,87 @@
+
+song_lyrics_quotes = [
+    "You’ve got enemies? Good, that means you actually stood up for something. – Eminem",
+
+    "You gotta be able to smile through the bullshit. – Tupac",
+
+    "I can see your sad even when you smile even when you laugh I can see if in your eyes deep inside you wanna cry. – Eminem",
+
+    "And I’m thankful for everyday that I’m givin’Both the easy and hard ones I’m livin",
+
+    "It isn’t where I am, It’s only where I’ll go from here",
+
+    'Times Are Hard for Dreamers. - Amelie',
+
+    'Don’t hold me back ’cause today all my dreams will come true! ,Good Morning Baltimore- Hairspray',
+
+    'Emancipate yourselves from mental slavery.None but ourselves can free our minds. ― Bob Marley',
+
+    "Don't criticize what you can't understand. ― Bob Dylan",
+
+    'funny how a beautiful song could tell such a sad story, ― Sarah Dessen, Lock and Key',
+
+    'The story of life is quicker than the wink of an eye, the story of love is hello and goodbye...until we meet again ― Jimi Hendrix',
+
+    "you're an expert at sorry and keeping the lines blurry ― Taylor Swift",
+
+    'Would you destroy Something perfect in order to make it beautiful?― Gerard Way',
+
+    "Get up, stand up, Stand up for your rights. Get up, stand up, Don't give up the fight. ― Bob Marley, Bob Marley - Legend",
+
+    'We all shine on...like the moon and the stars and the sun...we all shine on...come on and on and on...― john lennon',
+
+    "Close your eyes and I'll kiss you, Tomorrow I'll miss you.― Paul McCartney",
+
+    "When pain brings you down, don't be silly, don't close your eyes and cry, you just might be in the best position to see the sun shine. ― Alanis Morissette",
+
+    'Excuse me while I kiss the sky. ― Jimi Hendrix',
+
+    'You have every right to a beautiful life. ― Selena Gomez',
+
+    "You don't know you're beautiful. ― One Direction",
+
+    'Work it, make it, do it, makes us: harder, better, faster, stronger – Daft Punk Harder, Better, Faster',
+
+    'Sing with me, sing for the year, sing for the laughter and sing it for the tear. – Aerosmith “Dream On',
+
+    'Would you destroy Something perfect in order to make it beautiful? ― Gerard Way',
+
+    'I think there is a song out there to describe just about any situation. ― Criss Jami, Killosophy',
+
+    'When pain brings you down, don’t be silly, don’t close your eyes and cry, you just might be in the best position to see the sun shine. ― Alanis Morissette',
+
+    'Love me or hate me, i swear it won’t make or break me. ― Lil Wayne',
+
+    'It’s my life and it’s now or never! Cause I ain’t gonna live forever, I just want live while I’m alive. It’s my life! – Bon Jovi “It’s My Life',
+
+    'Get up, stand up, Stand up for your rights. Get up, stand up, Don’t give up the fight. ― Bob Marley, Bob Marley – Legend',
+
+    'And if at first your don’t succeed, then dust yourself off and try again! – Aaliyah Try Again',
+
+    'Don’t criticize what you can’t understand. ― Bob Dylan',
+
+    'I won’t be afraid just as long as you stand, stand by me. – Ben E. King Stand By Me',
+
+    'I’m a survivor, I’m not gonna give up, I’m not gonna, stop I’m gonna work harder. – Destiny’s Child “Survivor',
+
+    'The story of life is quicker than the wink of an eye, the story of love is hello and goodbye…until we meet again ― Jimi Hendrix',
+
+    'You’re an expert at sorry and keeping the lines blurry. ― Taylor Swift',
+
+    'I will survive! Oh as long as I know how to love I know I will stay alive! – Gloria Gaynor I Will Survive',
+
+    'We are the world, we are the children, we are the ones who make a brighter day, so let’s start giving. – United Support of Artists for Africa We Are The World',
+
+    'Every little thing is gonna be alright! – Bob Marley',
+
+    'Don’t give up. You’ve got a reason to live. Can’t forget you only get what you give. – New Radicals “You Get What You Give',
+
+    'You may say I’m a dreamer, but I’m not the only one. – John Lennon, Imagine',
+
+    'You only get one shot, do not miss your chance to blow. This opportunity comes once in a lifetime – Eminem Lose Yourself',
+
+    'I’d rather laugh with the sinners than cry with the saints, the sinners are much more fun. ― Billy Joel',
+
+    'Don’t stop, never give up. Hold your head high and reach the top. Let the world see what you have got. Bring it all back to you. – S Club 7 ,Bring it all back'
+]
+
